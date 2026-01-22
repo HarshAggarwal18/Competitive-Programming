c93951298d169f9096bb5ad851b6a713bc249411
harsh <harshgrwl18@gmail.com>
2026-01-22 09:33:04 +0530

    Day 3 -1 Done

---
+++ 01 Implementation/TheNewYear.java
@@ -0,0 +1,13 @@
+public class TheNewYear {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int x1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int x3 = sc.nextInt();
+        int abs1 = Math.abs(x1 - x2);
+        int abs2 = Math.abs(x2 - x3);
+        int abs3 = Math.abs(x1 - x3);
+        int res = Math.max(abs1, Math.max(abs2, abs3));
+        System.out.println(res);
+    }
+}
