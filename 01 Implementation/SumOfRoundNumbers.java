51abb4536315a183d1986657977c88cffafda0e6
harsh <harshgrwl18@gmail.com>
2026-01-22 10:11:08 +0530

    day 3 -2 omtimise

--- 01 Implementation/SumOfRoundNumbers.java
+++ 01 Implementation/SumOfRoundNumbers.java
@@ -1,33 +1,27 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
+import java.util.*;
 
 public class SumOfRoundNumbers {
     public static void main(String[] args) {
-        java.util.Scanner sc = new java.util.Scanner(System.in);
-        int q = sc.nextInt();
-        int []nums = new int[q];
-        for (int i = 0; i < q; i++) {
-            nums[i] = sc.nextInt();
-        }
-        while(q-- > 0){
-            int n = nums[nums.length -q -1];
-            int cnt = 0;
-            int nonZeroCount = 0;
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
             ArrayList<Integer> res = new ArrayList<>();
-            while(n > 0){
-                cnt++;
-                int rem = n % 10;
-                if(rem != 0){
-                    nonZeroCount++;
-                    int num = rem * (int)Math.pow(10, cnt -1);
-                    // System.out.print(num + " ");
-                    res.add(num);
-                }  
-                n = n / 10;    
+
+            int place = 1;
+            while (n > 0) {
+                int digit = n % 10;
+                if (digit != 0) {
+                    res.add(digit * place);
+                }
+                n /= 10;
+                place *= 10; // power of 10
             }
-            System.out.println(nonZeroCount);
-            for( int i : res){
-                System.out.print(i + " ");
+
+            System.out.println(res.size()); // non zero digits count
+            for (int x : res) {
+                System.out.print(x + " ");
             }
             System.out.println();
         }
