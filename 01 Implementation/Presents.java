262a266cd166cd1942af64a1233aebe008b3d954
harsh <harshgrwl18@gmail.com>
2026-01-25 21:00:23 +0530

    Day 5 -2 Done

---
+++ 01 Implementation/Presents.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Presents {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] a = new int[n];   // input array
+        int[] ans = new int[n]; // output array
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            ans[a[i] - 1] = i + 1;
+        }
+
+        System.out.println(Arrays.toString(ans));
+    }
+}
