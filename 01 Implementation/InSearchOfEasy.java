3ba81cef633be49fc19c8b75b07693a452cdc5c8
harsh <harshgrwl18@gmail.com>
2026-01-25 20:53:58 +0530

    Day 5 -1 Done

---
+++ 01 Implementation/InSearchOfEasy.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class InSearchOfEasy {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while (n != 0) {
+            // System.out.println(n);
+            int a = sc.nextInt();
+            if(a == 1) {
+                System.out.println("HARD");
+                return;
+            }
+        }
+        System.out.println("EASY");
+        sc.close();
+
+    }
+}
